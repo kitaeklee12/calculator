42d08111362940cabf8f71858cba7de18b70acdc
kitaeklee <https://github.com/kitaeklee12/git-practice.git>
2023-05-31 13:23:23 +0900

    Add Clear btn

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import sys
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-QMessageBox) # QMessageBox : 메시지박스 위젯
-from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
+                             QMessageBox, QPlainTextEdit, QHBoxLayout)
+from PyQt5.QtGui import QIcon
+
+
 class Calculator(QWidget):
-def __init__(self):
-super().__init__()
-self.initUI()
-def initUI(self):
-self.btn1=QPushButton('Message',self) # 버튼 추가
-self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
-vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
-vbox.addStretch(1) # 빈 공간
-vbox.addWidget(self.btn1) # 버튼 위치
-vbox.addStretch(1) # 빈 공간
-self.setLayout(vbox) # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 설정self.setWindowTitle('Calculator’)
-self.setWindowIcon(QIcon('icon.png')) # 윈도우 아이콘 추가
-self.resize(256,256)
-self.show()
-def activateMessage(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
-QMessageBox.information(self,"information","Button clicked!")
-if __name__ =='__main__’:
-app=QApplication(sys.argv)
-view=Calculator()
-sys.exit(app.exec_())
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
+        #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256, 256)
+        self.show()
+        
+    def activateMessage(self):
+        self.te1.appendPlainText("Button clicked!")
+        
+    def clearMessage(self):
+        self.te1.clear()
+        
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    view = Calculator()
+    sys.exit(app.exec_())
+    
